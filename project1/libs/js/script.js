b9ca14f34a184b6c26ae666f1e6447e7e75fdf81
Will h <127686961+willHarveyHarrison@users.noreply.github.com>
2024-07-16 16:26:47 +0100

    Add files via upload

--- project1/libs/js/script.js
+++ project1/libs/js/script.js
@@ -1,6 +1,5 @@
 import { tomtomKey } from './keys.js'; // Correct import syntax
-import { countriesGeoJson } from '../geojson/countries.js'
-import { countryGeo } from '../geojson/Country.js'
+import { countriesGeoJson } from '../geojson/countryBorders.geo.js'
 //map init
 var map = L.map('map');
 
@@ -34,14 +33,13 @@ streetView.addTo(map);
 // add tile control panel to map
 L.control.layers(baseMaps).addTo(map);
 // add country borders to map
-//this adds all countries to the map => L.geoJSON(countryGeo).addTo(map);
+//this adds all countries to the map => L.geoJSON(countriesGeoJson).addTo(map);
 
 
 // Function to filter and display the country
-
-function showCountry(countryName) {
-    var countryFeature = countryGeo.features.filter(function(feature) {
-        return feature.properties.SOVEREIGNT === countryName;
+function showCountry(country) {
+    var countryFeature = countriesGeoJson.features.filter(function(feature) {
+        return feature.properties.name === country;
     });
 
     // Add the filtered feature to the map
@@ -49,11 +47,61 @@ function showCountry(countryName) {
         onEachFeature: function (feature, layer) {
             layer.on({
                 click: function (e) {
-                    openCountryPanel(feature.properties.SOVEREIGNT);
+                    openCountryPanel(feature.properties.name);
                 }
             });
         }
     }).addTo(map);
 }
+//shows poligon for selected country
+showCountry("United States");
 
-showCountry("France");
+// Function to find the extrema for a specific country
+function findExtrema(geoJson, countryName) {
+    let south = Infinity;
+    let west = Infinity;
+    let east = -Infinity;
+    let north = -Infinity;
+
+    let countryFeature = geoJson.features.find(feature => feature.properties.name === countryName);
+
+    if (countryFeature) {
+        if (countryFeature.geometry.type === "MultiPolygon") {
+            countryFeature.geometry.coordinates.forEach(polygon => {
+                polygon.forEach(ring => {
+                    ring.forEach(coord => {
+                        let [lon, lat] = coord;
+                        if (lat < south) south = lat;
+                        if (lat > north) north = lat;
+                        if (lon < west) west = lon;
+                        if (lon > east) east = lon;
+                    });
+                });
+            });
+        } else if (countryFeature.geometry.type === "Polygon") {
+            countryFeature.geometry.coordinates.forEach(ring => {
+                ring.forEach(coord => {
+                    let [lon, lat] = coord;
+                    if (lat < south) south = lat;
+                    if (lat > north) north = lat;
+                    if (lon < west) west = lon;
+                    if (lon > east) east = lon;
+                });
+            });
+        }
+    }
+
+    return { south, west, east, north };
+}
+
+// Find the extrema in the GeoJSON MultiPolygon for Australia
+var extrema = findExtrema(countriesGeoJson, "United States");
+console.log('Southernmost point:', extrema.south);
+console.log('Westernmost point:', extrema.west);
+console.log('Easternmost point:', extrema.east);
+console.log('Northernmost point:', extrema.north);
+
+map.fitBounds([
+    [extrema.south, extrema.west],
+    [extrema.north, extrema.east]
+]);
